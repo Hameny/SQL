f620ee21d0b52fd34f544450409ca6346228618a
Pavel Khamenia <pavel.2006.91@mail.ru>
2022-03-17 20:44:08 +0300

    Update file

--- HW2_SQL_DDL.sql
+++ HW2_SQL_DDL.sql
@@ -1,10 +1,10 @@
---1.Создать таблицу employees
+--1.Create table employees
 create table employees(
 	id serial primary key,
 	employee_name varchar(50) not null
 );
 
---2.Наполнить таблицу employee 70 строками.
+--2.Fill in the tables employee 70 lines.
 insert into employees(employee_name)
 values('Stanley'),
 	  ('Tadeo'),
@@ -77,13 +77,13 @@ values('Stanley'),
 	  ('Xavier'),
 	  ('Uria');
 
---3.Создать таблицу salary
+--3.Create table salary
 create table salary1(
 	id serial primary key,
 	monthly_salary1 int not null
 ); 
 
---4.Наполнить таблицу salary 15 строками:
+--4.Fill in the table salary 15 lines:
 insert into salary1(monthly_salary1)
 values(1000),
 	  (1100),
@@ -102,7 +102,7 @@ values(1000),
 	  (2400),
 	  (2500);
 
---5.Создать таблицу employee_salary
+--5.Create table employee_salary
 create table employee_salary(
 	id serial primary key,
 	employee_id int not null,
@@ -113,8 +113,7 @@ create table employee_salary(
 		references salary1(id)
 );
 
---6.Наполнить таблицу employee_salary 40 строками:
--- в 10 строк из 40 вставить несуществующие employee_id
+--6.Fill in the table employee_salary 40 lines:
 insert into employee_salary(employee_id,salary_id)
 values(3, 7),
       (1, 4),
@@ -159,7 +158,7 @@ values(3, 7),
 
 select * from employee_salary ;
 	
-	--7.	Создать таблицу roles1
+	--7.Create table roles1
 create table roles1(
 	id serial  primary key,
     role_name int not null unique
@@ -167,13 +166,13 @@ create table roles1(
 
 select * from roles1;
 
---8.Gоменять тип столба role_name с int на varchar(30)
+--8.Change the post type role_name  int to varchar(30)
 alter table roles1 
 alter column role_name type varchar(30) using role_name::varchar(30);
 
 select * from roles1;
 
---9.Наполнить таблицу roles 20 строками
+--9.Fill in the table roles 20 lines
 insert into roles1(role_name)
 values('Junior Python developer'),
       ('Middle Python developer'),
@@ -197,7 +196,7 @@ values('Junior Python developer'),
 
 select * from roles1;
 
---10.	Создать таблицу roles_employee
+--10.Create table roles_employee
 create table roles_employee(
 	id serial primary key,
 	employee_id int not null unique,
@@ -208,7 +207,7 @@ create table roles_employee(
 	
 select * from roles_employee  ;
 
---11.Наполнить таблицу roles_employee 40 строками:
+--11.Fill in the table roles_employee 40 lines:
 insert into roles_employee(employee_id, role_id1)
 values(6, 1),
 	  (7, 2),
